c357d2b0bff0471d98f0c3a405dcf373e5336c9e
Scott Persinger <scottpersinger@gmail.com>
2022-07-01 15:31:32 -0700

    Started adding tests including MockSvc

--- tests/test_pagers.py
+++ tests/test_pagers.py
@@ -10,20 +10,27 @@ def test_pagers():
             {"strategy": "pageAndCount", "page_size":25}
         )
 
-    pager = PagingHelper.get_pager(
+    pager1 = PagingHelper.get_pager(
             {"strategy": "pageAndCount", "page_size":25, "page_param": "page", "count_param" : "count"}
         )
 
-    assert isinstance(pager, PageAndCountPager)
-    assert pager.page_size == 25
+    assert isinstance(pager1, PageAndCountPager)
+    assert pager1.page_size == 25
 
-    pager = PagingHelper.get_pager(
+    pager2 = PagingHelper.get_pager(
         {"strategy": "offsetAndCount", "offset_param":"start", "count_param":"count", "page_size":100}
     )
-    assert isinstance(pager, OffsetAndCountPager)
-    assert pager.page_size == 100
+    assert isinstance(pager2, OffsetAndCountPager)
+    assert pager2.page_size == 100
 
     pager = PagingHelper.get_pager(
         {"strategy": "offsetAndCount", "offset_param":"start", "count_param":"count"}
     )
     assert pager.page_size == 1
+
+    # Use the pager as intended
+    for page in range(3):
+        params = pager1.get_request_params(page)        
+        assert params["page"] == page
+        assert params["count"] == pager.page_size
+
